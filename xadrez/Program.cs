044ffb5dd062d89d0e88045dd75e84a30f68093d
Wellington César <wellingtoncgs30@gmail.com>
2019-08-02 15:06:31 -0300

    Foi criado a classe Posicao no projeto

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine($"Posição: {P}");
         }
     }
 }
